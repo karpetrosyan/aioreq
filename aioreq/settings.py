09b38f9cf22b2425c2037d7b5d5fccb63dbd0ec2
karosis88 <kar.petrosyanpy@gmail.com>
2022-10-06 17:11:38 +0300

    Add .pyc into .gitignore

--- aioreq/settings.py
+++ aioreq/settings.py
@@ -2,7 +2,7 @@ import logging
 
 LOGGER_NAME = 'aioreq'
 MAIN_LOGGER_LEVEL = logging.INFO
-# MAIN_LOGGER_LEVEL = 100
+MAIN_LOGGER_LEVEL = 100
 STREAM_HANDLER_LEVEL = logging.DEBUG
 FORMAT = '%(name)s | %(levelname)s | %(message)s | %(asctime)s'
 
