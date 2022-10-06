96c4c7053eb8920349c8387f041d87adc785124d
brutella <brutella@gmail.com>
2022-10-06 15:25:01 +0300

    Add docstring in protocol/http.py

--- aioreq/protocol/http.py
+++ aioreq/protocol/http.py
@@ -20,6 +20,17 @@ class Response:
             headers : dict,
             body : str,
             request : 'Request' = None):
+        """
+        Response initalization method
+
+        :param scheme_and_version: Version and scheme for http. For example HTTP/1.1
+        :param status: response code returned with response
+        :param status_message: message returned with response status code
+        :param headers: response headers for example, Connection : Keep-Alive if version lower than HTTP/1.1
+        :param body: response body
+        :param request: request which response is self
+        :returns: None
+        """
         self.scheme_and_version = scheme_and_version
         self.status = status
         self.status_message = status_message
@@ -51,6 +62,16 @@ class Request:
             path, 
             scheme_and_version = 'HTTP/1.1'
             ) -> 'Request':
+        """
+        Request initalization method
+
+        :param method: HTTP method (GET, POST, PUT, PATCH)
+        :param host: HTTP header host which contains host's domain
+        :param headers: HTTP headers
+        :param path: HTTP server endpoint path specified after top-level domain
+        :scheme_and_version: HTTP scheme and version where HTTP is scheme 1.1 is a version
+        :returns: None
+        """
         self.host    = host
         self.headers = headers
         self.method  = method
@@ -58,6 +79,11 @@ class Request:
         self.scheme_and_version = scheme_and_version
 
     def get_raw_request(self) -> str:
+        """
+        Bytearray to write in the transport
+
+        :returns: raw http request text type of bytearray
+        """
         return ('\r\n'.join((
                 f'{self.method} {self.path} {self.scheme_and_version}',
                 f'Host:   {self.host}',
@@ -81,10 +107,24 @@ class Request:
 class Client:
 
     def __init__(self, headers = {}):
+        """
+        Initalization method for Client, session like object
+
+        :param headers: HTTP headers that should be send with each request in this session
+        """
+
         self.connection_mapper = {}
         self.headers = headers
 
-    async def get(self, url, headers = {}):
+    async def get(self, url, headers = {}) -> Response:
+        """
+        Simulates http GET method
+
+        :param url: Url where should be request send
+        :param headers: Http headers which should be used in this GET request
+        :returns: Response object which represents returned by server response 
+        """
+
         splited_url = UrlParser.parse(url)
         transport, protocol = await self.make_connection(splited_url)
         request = Request(
@@ -103,6 +143,14 @@ class Client:
          
 
     async def make_connection(self, splited_url):
+        """
+        Getting connection from already opened connections, to perform Keep-Alive logic,
+        if these connections exists or create the new one and save into connection pool
+
+        :param splited_url: Url object which contains all url parts (scheme, version, subdomain, domain, ...)
+        :returns: (transport, protocol) which are objects returned by loop.create_connection method
+        """
+
         transport, protocol = self.connection_mapper.get(splited_url.get_url_for_dns(), (None, None))
 
         if transport and transport.is_closing():
@@ -126,9 +174,20 @@ class Client:
         return transport, protocol
     
     async def __aenter__(self):
+        """
+        Implements startpoint for Client
+        for keyword <with> supporting
+
+        :returns: Client object
+        """ 
         return self
 
     async def __aexit__(self, *args, **kwargs):
+        """
+        Closes session resources which are all transport
+        connections into connection pool
+        """
+
         for host, (transport, protocol) in self.connection_mapper.items():
             transport.close()
             log.info(f"Transport closed {transport=}")
