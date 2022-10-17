2dba1b1f51ebc72aa78cb2bb825c1b717865b15b
test <test@gmail.com>
2022-10-17 12:54:37 +0400

    Fix setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="aioreq",
+        version='0.0.1',
+        description="Async requests lib",
+        install_requires = [
+            'dnspython'
+            ],
+        packages = find_packages(),
+        include_package_data=True,
+        )
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/Desktop/aioreq/venv"
+set -gx VIRTUAL_ENV "/home/kar/Desktop/aioreq/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
