0fc81949fee6fce170a313a0a3c2149f8a303fbd
karosis88 <kar.petrosyanpy@gmail.com>
2022-11-03 18:20:02 +0400

    Fix some bugs, fix AsyncRequestsError raising

--- tests/test_aioreq/test_client.py
+++ tests/test_aioreq/test_client.py
@@ -3,6 +3,17 @@ import asyncio
 import aioreq
 import pytest_asyncio
 
+from aioreq.errors.requests import AsyncRequestsError
+
+@pytest.mark.asyncio
+async def test_few_requests(one_time_session, event_loop):
+
+    t1 = await one_time_session.get('https://www.facebook.com')
+    t2 = await one_time_session.get('https://www.google.com')
+    t3 = await one_time_session.get('https://www.youtube.com')
+    await asyncio.gather(t1, t2, t3)
+    assert t1.result().status == t2.result().status == t3.result().status
+
 @pytest.mark.asyncio
 async def test_normal_request(one_time_session):
     url = 'http://youtube.com'
@@ -41,5 +52,18 @@ async def test_ping(one_time_session,
                     server):
     response = await one_time_session.get(server)
     assert response.status == 200
+
+
+#@pytest.mark.asyncio
+#async def test_same_domain_requests_with_cache_connections(one_time_session_cached,
+#                                                           event_loop):
+#   
+#    loop = event_loop
+#    t1 = loop.create_task(one_time_session_cached.get('https://www.youtube.com'))
+#    t2 = loop.create_task(one_time_session_cached.get('https://www.youtube.com'))
+#    await asyncio.gather(t1, t2)
+#    print(t1, t2)
+#
+
 
 
