edccebf206ca24e75187b218a96fe9dcd56d8cc9
test <test@gmail.com>
2022-11-03 12:29:39 +0400

    Change version into setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
         name="aioreq",
-        version='0.0.2',
+        version='0.0.3',
         description="Async requests lib",
         install_requires = [
             'certifi'
