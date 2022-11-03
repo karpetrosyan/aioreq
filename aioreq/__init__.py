e5b8c69f7b67ea1d75a1d958f5dd02086ed2956b
karosis88 <kar.petrosyanpy@gmail.com>
2022-11-03 18:23:58 +0400

    Change event loop to uvloop

--- aioreq/__init__.py
+++ aioreq/__init__.py
@@ -1,3 +1,6 @@
+import asyncio
+import uvloop
+
 from .protocol import http
 from .protocol import headers
 from .errors import requests
@@ -8,4 +11,5 @@ from .parser import response_parser
 from .parser import url_parser
 from .socket import connection
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
