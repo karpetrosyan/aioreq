62a6f4adceec8f2be59801e9b37b3a734b427d7c
test <test@test.com>
2022-11-08 21:34:45 +0400

    Fix some performance problems with starting test server

--- tests/test_aioreq/test_client.py
+++ tests/test_aioreq/test_client.py
@@ -23,7 +23,7 @@ async def test_normal_request(one_time_session):
 async def test_moved_301(one_time_session):
     url = 'http://google.com'
     response = await one_time_session.get(url, retry=0, redirect=0)
-    assert response.status == 301
+    assert response.status in (301, 302)
 
 @pytest.mark.asyncio
 async def test_moving_301(one_time_session):
@@ -41,7 +41,7 @@ async def test_https_request(one_time_session):
 async def test_gzip_request(one_time_session,
                             get_gzip_url):
     expected = ('testgzip' * 100000).encode()
-    response = await one_time_session.get(get_gzip_url)
+    response = await asyncio.wait_for(one_time_session.get(get_gzip_url), timeout=3)
     assert 'content-encoding' in response.headers # if content-encoding exists
                                                   # then server sent encoded message!
     assert len(response.body) == len(expected)
@@ -49,7 +49,7 @@ async def test_gzip_request(one_time_session,
 @pytest.mark.asyncio
 async def test_ping(one_time_session,
                     server):
-    response = await one_time_session.get(server)
+    response = await asyncio.wait_for(one_time_session.get(server), timeout=3)
     assert response.status == 200
 
 @pytest.mark.asyncio
