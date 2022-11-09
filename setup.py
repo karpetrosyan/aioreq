a54c03d82b0f0f32869f30ad42b13478cc52dfc1
test <test@test.com>
2022-11-09 19:29:12 +0400

    v0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md').read()
 
 setup(
         name="aioreq",
-        version='0.0.4',
+        version='0.0.5',
         description="Async requests lib",
         install_requires = [
             'certifi',
