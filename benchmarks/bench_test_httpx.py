0d95dc9b44127827aabc82006a51feff16ed3347
karosis88 <kar.petrosyanpy@gmail.com>
2022-11-09 18:46:41 +0400

    Fixed dns caching, removed aiohttp from benchmark, add debugging tool for function execution logging

--- benchmarks/bench_test_httpx.py
+++ benchmarks/bench_test_httpx.py
@@ -4,12 +4,16 @@ import asyncio
 from benchmark_settings import REQUESTS_COUNT
 from benchmark_settings import REQUESTS_URL
 
+async def send_req(cl):
+    resp = await cl.get(REQUESTS_URL)
+    return resp.status_code
+
 async def main():
-    async with httpx.AsyncClient() as cl:
+    async with httpx.AsyncClient(follow_redirects=False) as cl:
         tasks = []
         for j in range(REQUESTS_COUNT):
-            tasks.append(cl.get(REQUESTS_URL, ))
-        await asyncio.gather(*tasks)
+            tasks.append(send_req(cl, ))
+        return await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     asyncio.run(main())
--- benchmarks/run_tests_functions.py
+++ benchmarks/run_tests_functions.py
@@ -3,7 +3,6 @@ import asyncio
 from time import time
 from time import perf_counter
 
-from bench_test_aiohttp import main as aiohttp_main
 from bench_test_aioreq import main as aioreq_main
 from bench_test_httpx import main as httpx_main
 from bench_test_requests import main as requests_main
@@ -18,25 +17,33 @@ def print_result(text):
         print(text)
 
 def run_benchmark_async(benchmark_name, function):
-    print('Run benchmark...')
-    loop = asyncio.new_event_loop()
-    t1 = perf_counter()
-    loop.run_until_complete(function())
-    t2 = perf_counter()
-    print_result(f"Function test for {benchmark_name} completed. Time spent: {t2-t1}")
+    try:
+        print('Run benchmark...')
+        loop = asyncio.new_event_loop()
+        t1 = perf_counter()
+        responses = loop.run_until_complete(function())
+        t2 = perf_counter()
+        print_result(
+                (f"Function test for {benchmark_name} completed. Time spent: {t2-t1} | "
+                 f"status 200 count: {len([code for code in responses if code == 200 or code == '200'])}"
+                 )
+        )
+    except BaseException as e:
+        print_result(f"{e} was raised for {benchmark_name}")
+        
 
 def run_benchmark_sync(benchmark_name, function):
-    print('Run benchmark...')
-    t1 = perf_counter()
-    function()
-    t2 = perf_counter()
-    print_result(f"Function test for {benchmark_name} completed. Time spent: {t2-t1}")
+    try:
+        print('Run benchmark...')
+        t1 = perf_counter()
+        function()
+        t2 = perf_counter()
+        print_result(f"Function test for {benchmark_name} completed. Time spent: {t2-t1}")
+    except BaseException as e:
+        print_result(f"{e} was raised for {benchmark_name}")
 
 async_test_configs = {
-    aiohttp_main : {
-        'benchmark_name' : 'aiohttp',
-        'function' : run_benchmark_async
-    },
+
     aioreq_main : {
         'benchmark_name' : 'aioreq',
         'function' : run_benchmark_async
@@ -59,8 +66,7 @@ if __name__ == '__main__':
                 benchmark_name = async_test_configs[md]['benchmark_name'],
                 function = md
             ),  
-            (requests_main, aiohttp_main, aioreq_main, httpx_main)
+            (requests_main, aioreq_main, httpx_main)
             )
-      
         
 
