8f546bb44ce09c22ba039b76b141b5b92615b350
karosis88 <kar.petrosyanpy@gmail.com>
2022-11-15 14:24:20 +0400

    Replace setup.py with pyproject.toml

---
+++ aioreq-0.0.6/aioreq/parser/request_parser.py
@@ -0,0 +1,86 @@
+import json as _json
+
+from abc import ABCMeta
+from abc import abstractmethod
+from typing import Iterable
+from ..utils import debug
+
+
+class BaseRequestParser:
+    """
+    For parsing Request object to raw data which can be sent
+    via socket
+    """
+
+    @abstractmethod
+    def parse(cls, request: 'Request') -> str: ...  # type: ignore
+
+    @classmethod
+    def sum_path_parameters(cls, parameters: Iterable[Iterable[str]]):
+        return "&".join([f"{key}={value}" for key, value in parameters])
+
+
+class RequestParser(BaseRequestParser):
+    @classmethod
+    @debug.timer
+    def parse(cls, request: 'Request') -> str:  # type: ignore
+        """
+        Parsing object type of request to string representing HTTP message
+
+        :returns: raw http request text
+        :rtype: str
+        """
+
+        if request.path_parameters:
+            request.path += '?' + \
+                            cls.sum_path_parameters(request.path_parameters)
+
+        if request.content:
+            request.headers['Content-Length'] = len(request.content)
+
+        message = ('\r\n'.join((
+            f'{request.method} {request.path} {request.scheme_and_version}',
+            f'host:  {request.host.split("://", 1)[1]}',
+            *(f"{key}:  {value}" for key, value in request.headers.items()),
+        )) + '\r\n\r\n')
+
+        if type(request.content) in (bytes, bytearray):
+            request.content = request.content.decode()
+        message += request.content or ''
+
+        return message
+
+
+class JsonRequestParser(BaseRequestParser):
+
+    @classmethod
+    @debug.timer
+    def parse(cls, request: 'Request') -> str:  # type: ignore
+        """
+        Parsing object type of request to string representing HTTP message
+
+        :returns: raw http request text
+        :rtype: str
+        """
+
+        if type(request.content) in (bytes, bytearray):
+            request.content = request.content.decode()
+
+        if request.path_parameters:
+            request.path += '?' + \
+                            cls.sum_path_parameters(request.path_parameters)
+
+        if request.content:
+            request.content = _json.dumps(request.content)[1:-1]
+            request.headers['Content-Type'] = 'application/json'
+            request.headers['Content-Length'] = len(request.content)
+
+        message = ('\r\n'.join((
+            f'{request.method} {request.path} {request.scheme_and_version}',
+            f'host:  {request.host.split("://", 1)[1]}',
+            *(f"{key}:  {value}" for key, value in request.headers.items()),
+        )) + '\r\n\r\n')
+
+        message += request.content or ''
+
+        return message
---
+++ aioreq-0.0.6/aioreq/settings.py
@@ -0,0 +1,83 @@
+import logging
+
+from pathlib import Path
+from configparser import ConfigParser
+
+ini_file_path_posix = Path(__file__).parent / 'settings.ini'
+ini_file_path = str(ini_file_path_posix.absolute())
+
+parser = ConfigParser()
+parser.read(ini_file_path)
+
+log_level_mapper = {
+        'notset': logging.NOTSET,
+        'debug' : logging.DEBUG,
+        'info'  : logging.INFO,
+        'warning' : logging.WARNING,
+        'error' : logging.ERROR,
+        'critical' : logging.CRITICAL
+        }
+
+log_format_mapper = {
+        '$name': '%(name)s',
+        '$levelno' : '%(levelno)s',
+        '$levelname' : '%(levelname)s',
+        '$pathname' : '%(pathname)s',
+        '$filename' : '%(filename)s',
+        '$module' : '%(module)s',
+        '$lineno' : '%(lineno)d',
+        '$funcName' : '%(funcName)s',
+        '$created' : '%(created)f',
+        '$asctime' : '%(asctime)s',
+        '$msecs' : '%(msecs)d',
+        '$relativeCreated' : '%(relativeCreated)d',
+        '$thread' : "%(thread)d",
+        '$threadName' : '%(threadName)s',
+        '$process' : '%(process)d',
+        '$message' : '%(message)s'
+        }
+
+LOGGER_NAME = parser.get('Logging', 'logger_name')
+
+MAIN_LOGGER_LEVEL = parser.get('Logging', 'logger_level')
+STREAM_HANDLER_LEVEL = parser.get('Logging', 'stream_handler_level')
+
+REQUEST_REDIRECT_COUNT = parser.getint('Aioreq', 'request_redirect_count')
+REQUEST_RETRY_COUNT = parser.getint('Aioreq', 'request_retry_count')
+
+TEST_SERVER_DOMAIN = parser.get('Tests', 'test_server_domain')
+
+DEFAULT_TIMEOUT = parser.getint('Aioreq', 'request_timeout')
+
+if any(
+        (
+        (MAIN_LOGGER_LEVEL not in log_level_mapper),
+        (STREAM_HANDLER_LEVEL not in log_level_mapper)
+        )
+    ):
+    raise ValueError('Setting.ini contains invalid value '
+                     f'for one of the logger levels ({MAIN_LOGGER_LEVEL} or {STREAM_HANDLER_LEVEL})')
+
+MAIN_LOGGER_LEVEL = log_level_mapper.get(MAIN_LOGGER_LEVEL) # type: ignore
+STREAM_HANDLER_LEVEL = log_level_mapper.get(STREAM_HANDLER_LEVEL) # type: ignore
+
+FORMAT = parser.get('Logging', 'stream_handler_format')
+
+for key, value in log_format_mapper.items():
+    FORMAT = FORMAT.replace(key, value)
+
+
+DEFAULT_CONNECTION_TIMEOUT = int(parser.getfloat ('Connection', 'default_connection_timeout'))
+DEFAULT_DNS_SERVER = parser['Connection']['default_dns_server']
+
+
+main_logger = logging.getLogger(LOGGER_NAME)
+main_logger.setLevel(MAIN_LOGGER_LEVEL)
+
+handler = logging.StreamHandler()
+handler.setLevel(STREAM_HANDLER_LEVEL)
+
+formatter = logging.Formatter(FORMAT)
+
+handler.setFormatter(formatter)
+main_logger.addHandler(handler)
