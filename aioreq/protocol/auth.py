18b2cb93d31f117dcf810ff68d27ea154803ecbf
karosis88 <kar.petrosyanpy@gmail.com>
2022-12-29 11:21:07 +0400

    Add authentication schemes enum, basic authenticate support

---
+++ aioreq/protocol/auth.py
@@ -0,0 +1,31 @@
+from .headers import AuthenticationWWW
+from base64 import b64encode
+from enum import Enum
+
+
+def authenticate_basic(params, request) -> str:
+    return 'Basic ' + b64encode(f"{request.auth[0]}:{request.auth[1]}".encode()).decode()
+
+
+def authenticate_digest(self, params, request) -> str:
+    raise NotImplementedError()
+
+
+class AuthenticationSchemes(Enum):
+    BASIC = 'Basic'
+    DIGEST = 'Digest'
+
+    def authenticate(self, *args, **kwargs):
+
+        match self:
+            case AuthenticationSchemes.BASIC:
+                return authenticate_basic(*args)
+            case AuthenticationSchemes.DIGEST:
+                return authenticate_digest(**kwargs)
+
+
+def parse_auth_header(header: AuthenticationWWW, request):
+    authentication_schemes = [AuthenticationSchemes(scheme) for scheme in header.auth_schemes]
+
+    for authentication_scheme in authentication_schemes:
+        yield authentication_scheme.authenticate(header.auth_schemes[authentication_scheme.value], request)
