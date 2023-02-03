df758ed98a087445edee68837e108c81baf5f102
karosis88 <kar.petrosyanpy@gmail.com>
2023-02-03 11:29:15 +0400

    Make utils wrap_errors a context manager

--- aioreq/utils/generic.py
+++ aioreq/utils/generic.py
@@ -5,16 +5,17 @@ from typing import Awaitable
 from ..errors.base import UnexpectedError
 from ..errors.requests import RequestTimeoutError
 from ..settings import LOGGER_NAME
+import contextlib
 
 log = logging.getLogger(LOGGER_NAME)
 
 
-async def wrap_errors(coro: Awaitable):
+@contextlib.contextmanager
+def wrap_errors():
 
     try:
-        return await coro
-    except asyncio.exceptions.TimeoutError:
-        raise RequestTimeoutError(f"Request timeout error")
-    except Exception as e:
-        log.critical(e)
-        raise UnexpectedError(str(e))
+        yield
+    except asyncio.TimeoutError:
+        raise RequestTimeoutError
+
+
