d2f7f4e5d3f0ae7b1c887be423e7be4a2576740d
karosis88 <kar.petrosyanpy@gmail.com>
2023-03-23 16:42:35 +0300

    Drop doctests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,17 +3,4 @@
 set -x
 ./scripts/check.sh
 
-uvicorn tests.server:app --log-level critical --port 7575 &
-
-while [[ ! $(curl -s http://127.0.0.1:7575) ]]; do
-  sleep 2
-done
-
-pytest --doctest-modules --doctest-glob="*md" docs
-
-if [[ $(ps -p $PID > /dev/null) ]]
-then
-  kill -9 $!
-fi
-
 pytest
